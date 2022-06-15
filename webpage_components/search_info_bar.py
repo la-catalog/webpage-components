a037358898c6fc5a6ce65b7a9c1b4a06cc6ad257
thiagola92 <thiagola92@gmail.com>
2022-06-15 07:06:33 -0300

    Fix variable used

--- webpage_components/search_info_bar.py
+++ webpage_components/search_info_bar.py
@@ -1,14 +1,14 @@
 import streamlit as st
 
 
-def search_info_bar(results: int, time_spent: int) -> str:
+def search_info_bar(results_quantity: int, time_spent: int) -> str:
     hits_col, time_spent_col, view_col = st.columns(3)
 
     with view_col:
         view_mode = st.radio("view mode", ["default", "json", "code"], horizontal=True)
 
     with hits_col:
-        st.text(f"🔍 results: {results}")
+        st.text(f"🔍 results: {results_quantity}")
 
     with time_spent_col:
         st.text(f"⏱️ time spent: {time_spent}")
