1bafe3822447aeea8959fdaa0ee4bedc08e86fee
thiagola92 <thiagola92@gmail.com>
2022-06-15 07:15:16 -0300

    Rename function and fix typing

--- webpage_components/display_prices.py
+++ webpage_components/display_prices.py
@@ -3,7 +3,7 @@ import streamlit as st
 from text_utility import stringfy
 
 
-def display_prices(prices: list[tuple(float, str)]) -> None:
+def display_prices(prices: list[tuple[float, str]]) -> None:
     values = [stringfy(amount) for amount, _ in prices]
     currencies = [currency for _, currency in prices]
     dataframe = pd.DataFrame(
