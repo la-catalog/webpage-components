6a18080ea800c492b49456984cd66bdff7011afd
thiagola92 <thiagola92@gmail.com>
2022-06-15 07:24:02 -0300

    Fix typing

--- webpage_components/display_attributes.py
+++ webpage_components/display_attributes.py
@@ -2,7 +2,7 @@ import pandas as pd
 import streamlit as st
 
 
-def display_attributes(attributes: list[tuple(str)], id_: str) -> None:
+def display_attributes(attributes: list[tuple[str, str]], id_: str) -> None:
     if st.checkbox(label="👁️ attributes", key=f"{id_}_attributes"):
         names = [name for name, _ in attributes]
         values = [value for _, value in attributes]
