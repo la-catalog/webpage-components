afd592d7a57696a4543a58254a9320664f3e5235
thiagola92 <thiagola92@gmail.com>
2022-06-16 08:25:12 -0300

    Add search_label as parameter so it can be used for more than one thing

--- webpage_components/search_bar.py
+++ webpage_components/search_bar.py
@@ -3,11 +3,11 @@ from collections.abc import Iterator
 import streamlit as st
 
 
-def search_bar(marketplaces: list) -> Iterator[str]:
-    search_col, marketplace_col = st.columns([3, 1])
+def search_bar(marketplaces: list, search_label: str = "") -> Iterator[str]:
+    search_col, marketplace_col = st.columns([7, 1])
 
     with search_col:
-        yield st.text_input(label="", placeholder="Write something here")
+        yield st.text_input(label=search_label)
 
     with marketplace_col:
-        yield st.selectbox(label="", options=marketplaces)
+        yield st.selectbox(label="Marketplace", options=marketplaces)
